745bd66529822005c727c4db3a268be3b7367f48
Lakshya Duhoon <2023.lakshyad@isu.ac.in>
2024-06-30 17:08:55 +0530

    component updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
 import NewsContent from './components/NewsContent';
 import apikey from './data/config';
 import Footer from './components/Footer';
 import SelectCategory from './components/SelectCategory';
+
+export const Theme = createContext();
 
 function App() {
 
@@ -14,6 +16,11 @@ function App() {
   let [newsResults, setNewsResults] = useState();
   let [loadMore, setLoadMore] = useState(20);
   let [isSelected, setIsSelected] = useState(false);
+  let [theme, setTheme] = useState('light');
+
+  function toggleTheme() {
+    setTheme(theme==='light'?'dark' : 'light');
+  }
 
   // fetch the news using the news API and storing them in an array
   useEffect(() => {
@@ -33,15 +40,17 @@ function App() {
 
   return (
     <>
-      {isSelected ? (
-        <div>
-          <NavBar setCategory={setCategory} />
-          <NewsContent setLoadMore={setLoadMore} loadMore={loadMore} newsArray={newsArray} newsResults={newsResults} />
-          <Footer />
-        </div>
-      ) : (
-        <SelectCategory setIsSelected={setIsSelected} setCategory={setCategory} />
-      )}
+      <Theme.Provider value={theme}>
+        {isSelected ? (
+          <div>
+            <NavBar setCategory={setCategory} toggleTheme={toggleTheme} />
+            <NewsContent setLoadMore={setLoadMore} loadMore={loadMore} newsArray={newsArray} newsResults={newsResults} />
+            <Footer />
+          </div>
+        ) : (
+          <SelectCategory setIsSelected={setIsSelected} setCategory={setCategory} />
+        )}
+      </Theme.Provider>
     </>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,19 @@
+import { useContext } from 'react';
 import MenuNav from './MenuNav'
 import './NavBar.css';
+import { Theme } from '../App';
 
 function NavBar(props) {
+
+    let theme=useContext(Theme);
     return (
         // setting the menu bar on the left hand side and the inshorts logo on the middle of the navigation bar
         <div className='container'>
-            <div className='icon'><MenuNav setCategory={props.setCategory} /></div>
+            <div><MenuNav setCategory={props.setCategory} /></div>
             <img src='https://assets.inshorts.com/website_assets/images/logo_inshorts.png'
                 alt='logo' className='img'
             />
+            {theme==='light'?(<button className='btn' onClick={props.toggleTheme}><i class="fa-regular fa-sun"></i></button>):(<button className='btn' onClick={props.toggleTheme}><i class="fa-solid fa-moon"></i></button>)}
         </div>
     )
 }
