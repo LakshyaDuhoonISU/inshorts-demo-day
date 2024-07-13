e42eade5c354d9f9d2d3c04103c3887e4632ee25
Lakshya Duhoon <2023.lakshyad@isu.ac.in>
2024-07-14 02:32:15 +0530

    error page added

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,12 +1,17 @@
+import { useNavigate } from 'react-router-dom'
 import './NotFound.css'
 
 function NotFound() {
+    let navigate = useNavigate();
     return (
         <div className='not-found-container'>
             <h1 className='not-found-heading '>404 - Not Found</h1>
             <p className='not-found-description'>
                 Oops! The page you're looking for does not exist.
             </p>
+            <button onClick={() => navigate('/')} className='go-back-button'>
+                Go Back to Categories
+            </button>
         </div>
     )
 }
